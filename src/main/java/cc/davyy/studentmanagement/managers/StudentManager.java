7d464d1d564c66b92bf73d9a202ed036880ad907
davide <dav.grigioni@gmail.com>
2024-08-23 22:27:11 +0200

    Added Text Utils from Apache Commons

--- src/main/java/cc/davyy/studentmanagement/managers/StudentManager.java
+++ src/main/java/cc/davyy/studentmanagement/managers/StudentManager.java
@@ -1,11 +1,11 @@
 package cc.davyy.studentmanagement.managers;
 
 import cc.davyy.studentmanagement.model.Student;
+import org.apache.commons.text.WordUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-import java.util.Scanner;
 
 /**
  * This class is responsible for managing students in a student management system.
@@ -14,42 +14,36 @@ import java.util.Scanner;
 public class StudentManager {
 
     private final List<Student> students = new ArrayList<>();
-    private final Scanner scanner = new Scanner(System.in);
 
     public void addStudent(Student student) {
-        if (students.stream().noneMatch(s -> s.id() == student.id())) {
-            students.add(student);
-            System.out.println("Student added successfully.");
-        } else {
+        if (students.stream()
+                .anyMatch(s -> s.id() == student.id())) {
             System.out.println("Student with ID " + student.id() + " already exists.");
-        }
-    }
-
-    public void viewStudent() {
-        if (students.isEmpty()) {
-            System.out.println("No students found.");
             return;
         }
 
-        System.out.println("Student List:");
-        students.forEach(System.out::println);
+        String capitalized = WordUtils.capitalize(student.name());
+        Student capitalizeStudent = new Student(student.id(), capitalized, student.age());
+        students.add(capitalizeStudent);
+        System.out.println("Student added successfully.");
     }
 
     public void deleteStudent(int id) {
         Optional<Student> student = findStudentById(id);
-        if (student.isPresent()) {
-            students.remove(student.get());
-            System.out.println("Student with ID " + id + " deleted successfully.");
-        } else {
+
+        if (student.isEmpty()) {
             System.out.println("Student with ID " + id + " not found.");
+            return;
         }
+
+        students.remove(student.get());
+        System.out.println("Student with ID " + id + " deleted successfully.");
     }
 
     public List<Student> getAllStudents() { return new ArrayList<>(students); }
 
     private Optional<Student> findStudentById(int id) {
-        return students
-                .stream()
+        return students.stream()
                 .filter(student -> student.id() == id)
                 .findFirst();
     }
